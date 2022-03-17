08818f1dfa099100ac413cd4324148f1a8c6dc0a
Đinh Văn Thức <77371672+DinhVanThuc1308@users.noreply.github.com>
2022-03-17 23:09:44 +0700

    Add files via upload

---
+++ HW3/Stackarr.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+ 
+class Stackarr{
+    public static void main(String[] agrs){
+        Stack <Integer> s = new Stack<Integer>();
+        for (int i=0; i<10; i++)
+            s.push(i);
+        System.out.println("Index of number 6 in Stack : " + s.search(6)); 
+        System.out.println("Index of number 15 in Stack : " + s.search(15));
+        while(!s.isEmpty()){
+            System.out.print(s.peek() + "   ");
+            s.pop();
+        }
+    }
+}
